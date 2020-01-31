78a9c7bffdc0126cd75067e58dc38da1d731cdd4
Kallebe18 <kallebegomes18@gmail.com>
2020-01-31 13:04:14 -0300

    Updates

---
+++ precache-manifest.90d046f5cb4239b07d40315c65205089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd77d635358ca3f0a104d70c2216eccd",
+    "url": "/Desafio-Grupo-Tesseract/frontend/index.html"
+  },
+  {
+    "revision": "dd55c5068bfe06e42a51",
+    "url": "/Desafio-Grupo-Tesseract/frontend/static/css/main.2faac570.chunk.css"
+  },
+  {
+    "revision": "f49edd8bc8f35812281c",
+    "url": "/Desafio-Grupo-Tesseract/frontend/static/js/2.40f6d40c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Desafio-Grupo-Tesseract/frontend/static/js/2.40f6d40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd55c5068bfe06e42a51",
+    "url": "/Desafio-Grupo-Tesseract/frontend/static/js/main.192a3367.chunk.js"
+  },
+  {
+    "revision": "b8e917111ba3284a8abb",
+    "url": "/Desafio-Grupo-Tesseract/frontend/static/js/runtime-main.51656176.js"
+  }
+]);
