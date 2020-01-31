5d34fff102f1e95ef343fc66ee16af96282e18d0
Kallebe18 <kallebegomes18@gmail.com>
2020-01-31 10:57:14 -0300

    Initiating members request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+import './globals.css';
+
+const Members = ({listOfMembers}) => {
+  return (
+    <ul>
+      {listOfMembers.map((member, index) => {
+        console.log(member);
+        return (
+          <li key={index}>{member.login}</li>
+        );
+      })}
+    </ul>
+  );
+}
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const res = await api.get('orgs/grupotesseract/public_members');
+      setMembers(res.data);
+    }
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
+      <h1>Listagem de Membros do Grupo Tesseract</h1>
+      <Members listOfMembers={members}/>
     </div>
   );
 }
