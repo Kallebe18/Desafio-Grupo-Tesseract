b10a519b4ca8529bed0b749750340b43784c0907
Kallebe18 <kallebegomes18@gmail.com>
2020-01-31 13:06:27 -0300

    Updates

---
+++ precache-manifest.075b78d900cadf5f5965b562fe87db3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099d37cd56c05b50c41cbec5c1d7d327",
+    "url": "/desafio-grupo-tesseract/index.html"
+  },
+  {
+    "revision": "dd55c5068bfe06e42a51",
+    "url": "/desafio-grupo-tesseract/static/css/main.2faac570.chunk.css"
+  },
+  {
+    "revision": "f49edd8bc8f35812281c",
+    "url": "/desafio-grupo-tesseract/static/js/2.40f6d40c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/desafio-grupo-tesseract/static/js/2.40f6d40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd55c5068bfe06e42a51",
+    "url": "/desafio-grupo-tesseract/static/js/main.192a3367.chunk.js"
+  },
+  {
+    "revision": "353b7a7e30944300b001",
+    "url": "/desafio-grupo-tesseract/static/js/runtime-main.fff4ed91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Desafio-Grupo-Tesseract/frontend/precache-manifest.90d046f5cb4239b07d40315c65205089.js"
+  "/desafio-grupo-tesseract/precache-manifest.075b78d900cadf5f5965b562fe87db3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Desafio-Grupo-Tesseract/frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/desafio-grupo-tesseract/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
